f6fe21900d2cae109f437a0862d72dc9ba67e9ba
Sergey <sawertion@gmail.com>
2023-10-20 15:02:46 +0200

    remove comment

--- db.py
+++ db.py
@@ -5,7 +5,7 @@ class SQL:
     def __init__(self):
         config = yaml.safe_load(open('./config.yml'))['mysql']
         self.db = pymysql.connect(
-		host=config['host'], # 2. write code to create a connection from. i.e. host=config['host']
+		host=config['host'],
                 user=config['user'],
 		password=config['password'],
 		database=config['database']
@@ -16,7 +16,7 @@ class SQL:
         self.db.close()
 
     def get_all_tasks(self):
-        sql = 'select * from Tasks' # 3. write sql query to get all from Tasks table
+        sql = 'select * from Tasks'
         with self.db.cursor() as cursor:
             cursor.execute(sql)
             return cursor.fetchall()
